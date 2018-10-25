62ca3b8696892a8950359b2554fab7752daaa0f1
SanjushaGuvvala <gvl.sanju@gmail.com>
2018-10-25 19:27:25 +0800

    25th October 2018

--- hyun_jeong_codes/nounPrase_summ.py
+++ hyun_jeong_codes/nounPrase_summ.py
@@ -6,9 +6,153 @@ import csv
 import collections
 import matplotlib.pyplot as plt
 
+from nltk import Tree, RegexpParser, ne_chunk
+
+nltk.download('maxent_ne_chunker')
+
 print('Reading Data...\n')
-tokens_df = pd.read_csv('pos_tag_out.csv', sep=',', encoding='utf-8')
+tokens_df = pd.read_csv('TokensDF.csv', sep=',', encoding='utf-8')
 print('Data READ\n')
 
-def noun_phrase_extraction(text, chunk_fn = nltk.ne_chunk()):
-    extracted = chunk_fn()
+print('Replacing NAN values with empty string\n')
+tokens_df = tokens_df.replace(np.nan, '', regex=True)
+print(tokens_df)
+
+print('Sentence Segmentation\n')
+#finding the length of reviewText in number of sentences
+seg_df = tokens_df
+seg_list = []
+for index in seg_df.index:
+    sentence = seg_df['reviewText'][index]
+    sentence_list = nltk.tokenize.sent_tokenize(sentence)
+    for sent in sentence_list:
+        seg_list.append(sent)
+
+print('Sentence Segmentation Completed\n')
+
+print("noun phrase extraction")
+#just carry out noun phrase extraction
+NP = "NP: {<DT>?<JJ>*<NN>}"
+extractor = nltk.RegexpParser(NP)
+
+npSentence = [nltk.word_tokenize(sent) for sent in seg_list]
+npSentence2 = [nltk.pos_tag(sent) for sent in npSentence]
+
+noun_phrases_list = [[' '.join(leaf[0] for leaf in tree.leaves())
+                      for tree in extractor.parse(sent).subtrees()
+                      if tree.label()=='NP']
+                      for sent in npSentence2]
+
+with open('nounPhrase.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in noun_phrases_list:
+        writer.writerow([i])
+
+#dictionary to count keys
+print('top 20 most frequent noun phrases')
+npCount = {}
+
+for s in noun_phrases_list:
+    for phrase in s:
+        if phrase in npCount:
+            if phrase == '':
+                break
+            npCount[phrase] += 1
+        else:
+            npCount[phrase] = 1
+
+print(npCount)
+
+print('get top 20 most frequent phrases')
+
+npCounter  = collections.Counter(npCount)
+for np, count in npCounter.most_common(20):
+    print(np, ": ", count)
+top20_phrases = npCounter.most_common(20)
+top20_phrases_df = pd.DataFrame(top20_phrases, columns = ['Noun Phrases', 'Count'])
+top20_phrases_df.plot.bar(x='Noun Phrases',y='Count', color='steelblue')
+plt.savefig('Top20_Noun_Phrases.png', bbox_inches='tight')
+
+
+#top 3 popular reviews -   B005SUHPO6, B0042FV2SI, B008OHNZI0
+print('extract 3 popular products')
+#extract top 3 popular products
+df1 = tokens_df.loc[tokens_df['productID'] == 'B005SUHPO6']
+df2 = tokens_df.loc[tokens_df['productID'] == 'B0042FV2SI']
+df3 = tokens_df.loc[tokens_df['productID'] == 'B008OHNZI0']
+
+#returns segmented sentences
+def sentenceSeg(df):
+    seg_df = df
+    seg_list = []
+    for index in seg_df.index:
+        sentence = seg_df['reviewText'][index]
+        sentence_list = nltk.tokenize.sent_tokenize(sentence)
+        for sent in sentence_list:
+            seg_list.append(sent)
+    print('Sentence Segmentation Completed\n')
+    return seg_list
+
+def npExtract(segmentedList):
+    #extract noun phrase first
+    NP = "NP: {<DT>?<JJ>*<NN>}"
+    extractor = nltk.RegexpParser(NP)
+
+    npSentence = [nltk.word_tokenize(sent) for sent in segmentedList]
+    npSentence2 = [nltk.pos_tag(sent) for sent in npSentence]
+
+    noun_phrases_list = [[' '.join(leaf[0] for leaf in tree.leaves())
+                          for tree in extractor.parse(sent).subtrees()
+                          if tree.label() == 'NP']
+                         for sent in npSentence2]
+    print(noun_phrases_list)
+    return noun_phrases_list
+
+def npRepresentative(noun_phrases_list, full_list_count):
+    npCount = {}
+    for s in noun_phrases_list:
+        for phrase in s:
+            if phrase in npCount:
+                if phrase == '':
+                    break
+                npCount[phrase] += 1
+            else:
+                npCount[phrase] = 1
+    print(npCount)
+    #if the phrase appears in other reviews, not very representative of the product.
+
+    for npKey in npCount.keys():
+        if npKey in full_list_count.keys():
+            npCount[npKey] = npCount[npKey] // full_list_count[npKey]
+
+    print('get top 10 most frequent phrases')
+    npCounter = collections.Counter(npCount)
+    for np, count in npCounter.most_common(10):
+        print(np, ": ", count)
+    top20_phrases = npCounter.most_common(10)
+    top20_phrases_df = pd.DataFrame(top20_phrases, columns=['Noun Phrases', 'Count'])
+    return top20_phrases_df
+
+seg1 = sentenceSeg(df1)
+seg2 = sentenceSeg(df2)
+seg3 = sentenceSeg(df3)
+
+np1 = npExtract(seg1)
+np2 = npExtract(seg2)
+np3 = npExtract(seg3)
+
+npr1 = npRepresentative(np1, npCount)
+npr2 = npRepresentative(np2, npCount)
+npr3 = npRepresentative(np3, npCount)
+
+print("first product-----------------------------------------------------")
+print(npr1)
+print('second product-----------------------------------------------------')
+print(npr2)
+print('third product------------------------------------------------------')
+print(npr3)
+
+npr1.to_csv('Popular_product_top10_np_1.csv', encoding='utf-8', index = False)
+npr2.to_csv('Popular_product_top10_np_2.csv', encoding='utf-8', index = False)
+npr2.to_csv('Popular_product_top10_np_2.csv', encoding='utf-8', index = False)
+##top 10 divided by np cound
---
+++ hyun_jeong_codes/random5_np.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import nltk
+import numpy as np
+import random
+import csv
+import collections
+import matplotlib.pyplot as plt
+
+from nltk import Tree, RegexpParser, ne_chunk
+
+nltk.download('maxent_ne_chunker')
+
+print('Reading Data...\n')
+tokens_df = pd.read_csv('TokensDF.csv', sep=',', encoding='utf-8')
+print('Data READ\n')
+
+print('extract 5 random reviews products')
+tokens_df1 = tokens_df.sample(n=1)
+tokens_df2 = tokens_df.sample(n=1)
+tokens_df3 = tokens_df.sample(n=1)
+tokens_df4 = tokens_df.sample(n=1)
+tokens_df5 = tokens_df.sample(n=1)
+
+#save the seleccted reviews
+frames = [tokens_df1, tokens_df2, tokens_df3, tokens_df4, tokens_df5]
+frames_merged = pd.concat(frames)
+#frames.to_csv('Random_sample_5_np.csv', encoding='utf-8', index = False)
+
+with open('Random5_np5.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in frames_merged:
+        writer.writerow([i])
+
+
+print('Replacing NAN values with empty string\n')
+tokens_df1 = tokens_df1.replace(np.nan, '', regex=True)
+tokens_df2 = tokens_df2.replace(np.nan, '', regex=True)
+tokens_df3 = tokens_df3.replace(np.nan, '', regex=True)
+tokens_df4 = tokens_df4.replace(np.nan, '', regex=True)
+tokens_df5 = tokens_df5.replace(np.nan, '', regex=True)
+
+print('first review----------------------------------------------')
+print(tokens_df1)
+print('second review----------------------------------------------')
+print(tokens_df2)
+print('third review----------------------------------------------')
+print(tokens_df3)
+print('fourth review----------------------------------------------')
+print(tokens_df4)
+print('fifth review----------------------------------------------')
+print(tokens_df5)
+
+#returns segmented sentences
+def sentenceSeg(df):
+    seg_df = df
+    seg_list = []
+    for index in seg_df.index:
+        sentence = seg_df['reviewText'][index]
+        sentence_list = nltk.tokenize.sent_tokenize(sentence)
+        for sent in sentence_list:
+            seg_list.append(sent)
+    print('Sentence Segmentation Completed\n')
+    return seg_list
+
+def npExtract(segmentedList):
+    #extract noun phrase first
+    NP = "NP: {<DT>?<JJ>*<NN>}"
+    extractor = nltk.RegexpParser(NP)
+
+    npSentence = [nltk.word_tokenize(sent) for sent in segmentedList]
+    npSentence2 = [nltk.pos_tag(sent) for sent in npSentence]
+
+    noun_phrases_list = [[' '.join(leaf[0] for leaf in tree.leaves())
+                          for tree in extractor.parse(sent).subtrees()
+                          if tree.label() == 'NP']
+                         for sent in npSentence2]
+    print(noun_phrases_list)
+    return noun_phrases_list
+
+extracted1 = npExtract(sentenceSeg(tokens_df1))
+extracted2 = npExtract(sentenceSeg(tokens_df2))
+extracted3 = npExtract(sentenceSeg(tokens_df3))
+extracted4 = npExtract(sentenceSeg(tokens_df4))
+extracted5 = npExtract(sentenceSeg(tokens_df5))
+
+with open('Random5_np1.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in extracted1:
+        writer.writerow([i])
+
+with open('Random5_np2.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in extracted2:
+        writer.writerow([i])
+
+with open('Random5_np3.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in extracted3:
+        writer.writerow([i])
+
+with open('Random5_np4.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in extracted4:
+        writer.writerow([i])
+
+with open('Random5_np5.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in extracted5:
+        writer.writerow([i])
